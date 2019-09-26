420ac8934ea01b2436a3a318a0fcab90f3c1df97
ecambero <47284284+ecambero@users.noreply.github.com>
2019-09-26 08:06:16 -0400

    Update

--- 12- Plotly (Interactive-Visualizations-and-Dashboards)/Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ 12- Plotly (Interactive-Visualizations-and-Dashboards)/Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,19 +1,25 @@
 function buildMetadata(sample) {
-
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
+  d3.json("../Instructions/DataSets/belly_button_metadata.csv").then((data) => {
     // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    PANEL.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    Object.entries(data).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
+    });
+    
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    buildGauge(data.WFREQ);
+  });
 }
+\Instructions\StarterCode\Belly_Button_Biodiversity\templates\index.html
+
+\Instructions\DataSets\belly_button_metadata.csv
 
 function buildCharts(sample) {
 
